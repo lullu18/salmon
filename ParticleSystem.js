08418f152d0adc86fc94e09400b38be6c4b0086e
lullu18 <limgayoon061108@gmail.com>
2025-11-16 15:29:20 +0900

    old 버전 최신 버전으로 바꾸기

---
+++ ParticleSystem.js
@@ -0,0 +1,27 @@
+// ParticleSystem class
+class ParticleSystem {
+    constructor(position) {
+        this.origin = position.copy();
+        this.particles = [];
+    }
+
+    addParticle() {
+        this.particles.push(new Particle(this.origin));
+    }
+    
+    setPosition(position) {
+        this.origin = position.copy();
+        this.particle
+    }
+
+    run() {
+        for (let i = this.particles.length-1; i >= 0; i--) {
+            let p = this.particles[i];
+            p.run();
+
+            if (p.isDead()) {
+                this.particles.splice(i, 1);
+            }
+        }
+    }
+}
